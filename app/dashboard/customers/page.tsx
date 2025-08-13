57f3288095dc516696c79d7dea00d5906bdaafa5
waliyinnura <waliyinnura@gmail.com>
2025-08-13 15:41:45 +0800

    feat: add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,40 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    // page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  //   const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        {/* <h1 className={`${lusitana.className} text-2xl`}>Customers</h1> */}
+        <Table customers={customers} />
+      </div>
+      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8"> */}
+      {/* <Search placeholder="Search customer..." /> */}
+      {/* <CreateInvoice /> */}
+      {/* </div> */}
+      {/* <Suspense key={query + currentPage} fallback={<Customers />}> */}
+      {/* </Suspense> */}
+      {/* <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div> */}
+    </div>
+  );
 }
